4ea20fc8b76b25528e47822fc7ad6a64dc9b4453
akash1012 <gupta.akash2090@gmail.com>
2025-02-14 20:44:12 +0530

    add user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users'
+
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,5 +12,15 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
+  get imagePath() {
+    return `assets/users/${this.selectedUser().avatar}`;
+  }
+
+  onSelectUser() {
+    console.log("clicked")
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
